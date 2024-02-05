2a1588e30547843e1f0830f932773e5eea68e8e8
mrarchis3d <49571076+mrarchis3d@users.noreply.github.com>
2024-02-05 02:14:04 -0500

    mayor changes products page, look and feel o main page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,18 @@ module.exports = {
 				custom:["Roboto", "sans-serif"]
 			}
 		},
+		fontSize: {
+			xs: '0.8rem',
+			sm: '1rem',
+			base: '1.25rem',
+			md: '1.25rem',
+			lg: '1.65rem',
+			xl: '2rem',
+			'2xl': '2.5rem',
+			'3xl': '3rem',
+			'4xl': '3.5rem',
+			'5xl': '3.9rem',
+		  }
 	},
 	plugins: [
 		require("@tailwindcss/typography"),
