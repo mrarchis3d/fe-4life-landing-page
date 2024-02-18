d4d443e7cd0aa50b4435277dc153b77078a8a2d0
mrarchis3d <49571076+mrarchis3d@users.noreply.github.com>
2024-02-17 21:37:15 -0500

    prettier run

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily:{
-				'sans':["Montserrat Alternates"],
-				custom:["Roboto", "sans-serif"]
-			}
-		},
-		fontSize: {
-			xs: '0.6rem',
-			sm: '0.8rem',
-			base: '1rem',
-			md: '1.25rem',
-			lg: '1.5rem',
-			xl: '2rem',
-			'2xl': '2.5rem',
-			'3xl': '3rem',
-			'4xl': '3.5rem',
-			'5xl': '3.9rem',
-		  }
-	},
-	plugins: [
-		require("@tailwindcss/typography"),
-		require("@tailwindcss/forms"),
-		require("@tailwindcss/line-clamp"),
-		require('@tailwindcss/aspect-ratio'),
-	],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat Alternates'],
+        custom: ['Roboto', 'sans-serif'],
+      },
+    },
+    fontSize: {
+      xs: '0.6rem',
+      sm: '0.8rem',
+      base: '1rem',
+      md: '1.25rem',
+      lg: '1.5rem',
+      xl: '2rem',
+      '2xl': '2.5rem',
+      '3xl': '3rem',
+      '4xl': '3.5rem',
+      '5xl': '3.9rem',
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
